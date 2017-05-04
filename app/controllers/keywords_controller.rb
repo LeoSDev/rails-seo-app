06196d18648e11d3fb71e3045b1689278afecdb7
Leo Sider <leo.sider@gmail.com>
2017-05-04 14:59:55 +0000

    CRUD and routes for keywords

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -1,2 +1,27 @@
 class KeywordsController < ApplicationController
+    
+    def index
+        @keywords = Keyword.all
+    end
+    
+    def show
+        @keywords = Keyword.find(params[:id])
+    end
+    
+    def new
+    end
+    
+    def create
+    end
+    
+    def edit
+    end
+    
+    def update
+    end
+    
+    def destroy
+    end
+
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,21 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-    get "/:keyword" => "pages#home"
+  resources :keywords, only:[:new, :create, :index, :show]
+    
+    # # Read action routes
+    # get "/keywords" => "keywords#index"
+    # get "/keywords/:id" => "keywords#show"
+    
+    # # Create action routes
+    # get "/keywords/new" => "keywords#new"
+    # post "/keywords" => "keywords#create"
+
+    # # Update action routes
+    # get "/keywords/:id/edit" => "keywords#edit"
+    # patch "/keywords/:id" => "keywords#update"
   
+    # # Delete action routes
+    # delete "/keywords/:id/delete" => "keywords#destroy"
+    
   end
